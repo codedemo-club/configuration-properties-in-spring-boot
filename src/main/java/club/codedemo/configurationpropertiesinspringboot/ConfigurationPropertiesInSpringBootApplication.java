83a74189edbe496bfbd1d5c2fd18c1430ad5ad26
teacherpan <3792535@qq.com>
2020-10-06 16:32:45 +0800

    init

---
+++ src/main/java/club/codedemo/configurationpropertiesinspringboot/ConfigurationPropertiesInSpringBootApplication.java
@@ -0,0 +1,13 @@
+package club.codedemo.configurationpropertiesinspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationPropertiesInSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigurationPropertiesInSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/club/codedemo/configurationpropertiesinspringboot/ConfigurationPropertiesInSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package club.codedemo.configurationpropertiesinspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigurationPropertiesInSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
